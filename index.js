b310a1299610adf6b050dfaf25b3faa3799ae97d
karthikrish06 <93328600+karthikrish06@users.noreply.github.com>
2024-09-23 20:07:01 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+import hotelRoutes from "./routes/hotels.js";
+import roomRoutes from "./routes/rooms.js";
+
+
+const app = express();
+dotenv.config();
+
+// middlewares
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
+
+app.use("/api/auth", authRoutes)
+app.use("/api/users", userRoutes)
+app.use("/api/hotels", hotelRoutes)
+app.use("/api/rooms", roomRoutes)
+
+app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "something went Wrong!";
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack,
+    });
+});
+
+//demoend point
+app.get("/", (req, res) => {
+    res.send("App working good");
+});
+
+
+// database connection
+const db_connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_DB_URL);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        throw error;
+    }
+}
+
+app.listen(8070, () => {
+    db_connect()
+    console.log(`server started in http://localhost:8070`);
+});
